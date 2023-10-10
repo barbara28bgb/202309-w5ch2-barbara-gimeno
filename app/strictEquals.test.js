92fd33ea030ea7828b1d1686741919f1303f0b10
BarbaraGB <barbaragimeno.cm@gmail.com>
2023-10-10 23:00:38 +0200

    Testing zero value

--- app/strictEquals.test.js
+++ app/strictEquals.test.js
@@ -13,3 +13,17 @@ describe("Given a scriptEqual function ", () => {
     });
   });
 });
+
+describe("Given a scriptEqual function ", () => {
+  describe("When recibes 0 and -0", () => {
+    test("Then it should retourn false", () => {
+      const valueA = 1;
+      const valueB = 1;
+      const isExpectStrictEqual = false;
+
+      const isStrictEqualValue = strictEquals(valueA, valueB);
+
+      expect(isStrictEqualValue).toBe(isExpectStrictEqual);
+    });
+  });
+});
